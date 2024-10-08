18538502f470eec5eaf54e3103e6f53748897859
Даниил <152735181+DrTECHNIC@users.noreply.github.com>
2024-10-08 11:32:07 +0300

    Delete 3 семестр/Архитектура вычислительных машин и систем/Prac3 directory

--- 3 семестр/Архитектура вычислительных машин и систем/Prac3/simulation/qsim/work/@prac3_vlg_check_tst/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity Prac3_vlg_check_tst is
-    port(
-        Y               : in     vl_logic;
-        sampler_rx      : in     vl_logic
-    );
-end Prac3_vlg_check_tst;
--- 3 семестр/Архитектура вычислительных машин и систем/Prac3/simulation/qsim/work/@prac3_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity Prac3_vlg_vec_tst is
-end Prac3_vlg_vec_tst;
