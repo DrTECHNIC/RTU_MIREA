450a0daa8cd53300774c59555a6820f50958a7a5
Даниил <152735181+DrTECHNIC@users.noreply.github.com>
2024-10-08 11:39:34 +0300

    Add files via upload

---
+++ 3 семестр/Архитектура вычислительных машин и систем/Prac3/simulation/qsim/work/@prac3_vlg_sample_tst/_primary.vhd
@@ -0,0 +1,18 @@
+library verilog;
+use verilog.vl_types.all;
+entity Prac3_vlg_sample_tst is
+    port(
+        A               : in     vl_logic;
+        B               : in     vl_logic;
+        C               : in     vl_logic;
+        D0              : in     vl_logic;
+        D1              : in     vl_logic;
+        D2              : in     vl_logic;
+        D3              : in     vl_logic;
+        D4              : in     vl_logic;
+        D5              : in     vl_logic;
+        D6              : in     vl_logic;
+        D7              : in     vl_logic;
+        sampler_tx      : out    vl_logic
+    );
+end Prac3_vlg_sample_tst;
