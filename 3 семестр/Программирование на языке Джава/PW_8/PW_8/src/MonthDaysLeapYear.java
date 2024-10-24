42e946381693f9db88c188df7976e8ecaf4d4f15
Даниил <152735181+DrTECHNIC@users.noreply.github.com>
2024-10-24 10:55:59 +0300

    Add files via upload

--- 3 семестр/Программирование на языке Джава/PW_8/PW_8/src/MonthDaysLeapYear.java
+++ 3 семестр/Программирование на языке Джава/PW_8/PW_8/src/MonthDaysLeapYear.java
@@ -29,5 +29,5 @@ public class MonthDaysLeapYear
     }
 
     public static boolean isLeapYear(int year)
-    { return year % 4 == 0; }
+    { return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0); }
 }
